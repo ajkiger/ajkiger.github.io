e8156d20336d5d99fef72d21d98e809b7c7b094f
ajkiger <ajkiger@gmail.com>
2014-05-26 20:37:22 -0700

    ggg

---
+++ webgl-utils.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2010, Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('S=4(){3 g=4(a){5\'\'+\'<n o="T-U: #V; W: v%; X: v%; Y-Z: 10;"><w>\'+\'<x y="11">\'+\'<9 o="12: n-13; 14-y: 15;">\'+\'<9 o="">\'+a+\'</9>\'+\'</9>\'+\'</x></w></n>\'};3 h=\'\'+\'16 17 z a A 18 19 p.<2/>\'+\'<a l="B://C.6.D">E F G 1a H A.</a>\';3 i=\'\'+"7 q 1b z p G 4.<2/>"+"1c 1d\'t 1e H 1f 1g 1h p.<2/>"+\'<a l="B://C.6.D/1i/">E F r 1j 1k.</a>\'+"<2/><2/><2/>7 q & 7 I 1l 1m r<2/>1n 1o 1p 1q 1r 1s.<2/><2/>"+\'<a l="J://K.L.M/N/O/s-1t-P-s-1u/1v?Q=8">7 q</a>\'+"<2/><2/>"+\'<a l="J://K.L.M/N/O/s-1w/1x?Q=8">7 I</a>\';3 j=4(c,d){4 u(a){3 b=c.1y;m(b){b.1z=g(a)}};m(!1.1A){u(h);5 R}3 e=k(c,d);m(!e){u(i)}5 e};3 k=4(a,b){3 c=["6","1B-6","1C-P","1D-6"];3 d=R;r(3 f=0;f<c.1E;++f){1F{d=a.1G(c[f],b)}1H(e){}m(d){1I}}5 d};5{1J:k,1K:j}}();1.1L=(4(){5 1.1M||1.1N||1.1O||1.1P||1.1Q||4(a,b){5 1.1R(a,1S/1T)}})();1.1U=(4(){5 1.1V||1.1W||1.1X||1.1Y||1.1Z||1.20})();',62,125,'|window|br|var|function|return|webgl|Word||div||||||||||||href|if|table|style|WebGL|Cubed|for|word||showLink|100|tr|td|align|requires|browser|http|get|org|Click|here|to|your|Head|https|itunes|apple|com|us|app|3d|mt|null|WebGLUtils|background|color|8CE|width|height|font|size|28px|center|display|cell|vertical|middle|This|page|that|supports|upgrade|Weekly|It|doesn|appear|device|can|support|troubleshooting|more|information|are|available|iOS|devices|in|the|App|Store|cubed|search|id672401704|head|id561785863|parentNode|innerHTML|WebGLRenderingContext|experimental|webkit|moz|length|try|getContext|catch|break|create3DContext|setupWebGL|requestAnimFrame|requestAnimationFrame|webkitRequestAnimationFrame|mozRequestAnimationFrame|oRequestAnimationFrame|msRequestAnimationFrame|setTimeout|1000|60|cancelAnimFrame|cancelAnimationFrame|webkitCancelAnimationFrame|mozCancelAnimationFrame|oCancelAnimationFrame|msCancelAnimationFrame|clearTimeout'.split('|'),0,{}))
