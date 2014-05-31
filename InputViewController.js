197c2d99dda51fdbcee848f39de951f085c3ae1b
ajkiger <ajkiger@gmail.com>
2014-05-31 14:30:55 -0700

    dd

---
+++ InputViewController.js
@@ -0,0 +1,10 @@
+/*
+Copyright 2014 JWACK APPS
+All rights reserved
+jwackapps.com
+Created by Andrew B. Kiger
+ajkiger@gmail.com
+*/
+
+
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('5 6=1;9 v(a){5 b=m.n||g.o.h||g.p.h;5 c=a.q-(b-r)/2;5 d=a.s-t-u;c=i(j.k(c*4)/4).l(0);d=i(j.k(d*4)/4).l(0);6=w;x(a);7(a.3)a.3();e a.f=1;8 1}9 y(a){7(!6){8}5 b=m.n||g.o.h||g.p.h;5 c=a.q-(b-r)/2;5 d=a.s-t-u;c=i(j.k(c*4)/4).l(0);d=i(j.k(d*4)/4).l(0);z(a);7(a.3)a.3();e a.f=1;8 1}9 A(a){6=1;B();7(a.3)a.3();e a.f=1;8 1}9 C(a){6=1;D();7(a.3)a.3();e a.f=1;8 1}9 E(a){6=1;7(a.3)a.3();e a.f=1;8 1}',41,41,'|false||preventDefault|100|var|mouseDown|if|return|function|||||else|returnValue|document|clientWidth|parseFloat|Math|round|toFixed|window|innerWidth|documentElement|body|clientX|640|clientY|96|25|handleMouseDownonCanvas|true|touchesBegan|handleMouseMoveonCanvas|touchesMoved|handleMouseUponCanvas|touchesEnd|handleMouseDblClickonCanvas|doubleTap|handleMouseOutonCanvas'.split('|'),0,{}))
---
+++ webgl-utils.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2010, Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('14=4(){5 g=4(a){7\'\'+\'<A B="15-16: #17; l: F%; m: F%; 18-19: 1a;"><G>\'+\'<H I="1b">\'+\'<o B="1c: A-1d; 1e-I: 1f;">\'+\'<o B="">\'+a+\'</o>\'+\'</o>\'+\'</H></G></A>\'};5 h=\'\'+"6 9 J K C D 4.<2/>"+\'L M://N.n.O D 1g P 1h.\'+\'<2/><2/><2/><2/>Q 6 R S p<2/>T U V W X Y.<2/><2/>\'+\'<q r="s" u="v/Z.w" l="8" m="8" x=" 6 9 ">&1&1&1&1&1&1<q r="s" u="v/10-11.w" l="8" m="8" x=" 6 y ">\'+\'<2/>6 9&1&1&1&1&1&1&12 y<2/>\';5 i=\'\'+"6 9 J K C D 4.<2/>"+"1i 1j\'t 1k P 1l 1m 1n C.<2/>"+\'L M://N.n.O p 1o 1p.\'+\'<2/><2/><2/><2/>Q 6 R S p<2/>T U V W X Y.<2/><2/>\'+\'<q r="s" u="v/Z.w" l="8" m="8" x=" 6 9 ">&1&1&1&1&1&1<q r="s" u="v/10-11.w" l="8" m="8" x=" 6 y ">\'+\'<2/>6 9&1&1&1&1&1&1&12 y<2/>\';5 j=4(c,d){4 E(a){5 b=c.1q;z(b){b.1r=g(a)}};z(!3.1s){E(h);7 13}5 e=k(c,d);z(!e){E(i)}7 e};5 k=4(a,b){5 c=["n","1t-n","1u-1v","1w-n"];5 d=13;p(5 f=0;f<c.1x;++f){1y{d=a.1z(c[f],b)}1A(e){}z(d){1B}}7 d};7{1C:k,1D:j}}();3.1E=(4(){7 3.1F||3.1G||3.1H||3.1I||3.1J||4(a,b){7 3.1K(a,1L/1M)}})();3.1N=(4(){7 3.1O||3.1P||3.1Q||3.1R||3.1S||3.1T})();',62,118,'|nbsp|br|window|function|var|Word|return|152|Cubed||||||||||||width|height|webgl|div|for|img|alt|Hints||src|images|png|title|Head|if|table|style|WebGL|to|showLink|100|tr|td|align|Weekly|requires|Visit|http|get|org|your|3D|Games|available|iOS|devices|in|the|App|Store|WCIcon152x152|icon03|152x152|nbspWord|null|WebGLUtils|background|color|8CE|font|size|28px|center|display|cell|vertical|middle|upgrade|browser|It|doesn|appear|device|can|support|more|information|parentNode|innerHTML|WebGLRenderingContext|experimental|webkit|3d|moz|length|try|getContext|catch|break|create3DContext|setupWebGL|requestAnimFrame|requestAnimationFrame|webkitRequestAnimationFrame|mozRequestAnimationFrame|oRequestAnimationFrame|msRequestAnimationFrame|setTimeout|1000|60|cancelAnimFrame|cancelAnimationFrame|webkitCancelAnimationFrame|mozCancelAnimationFrame|oCancelAnimationFrame|msCancelAnimationFrame|clearTimeout'.split('|'),0,{}))
