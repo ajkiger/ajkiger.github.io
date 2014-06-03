0ced76a26b96003ada58458b2051006eda90cfed
ajkiger <ajkiger@gmail.com>
2014-06-02 19:23:45 -0700

    gg

--- test01/CubeObject.js
+++ test01/CubeObject.js
@@ -1126,7 +1126,7 @@ function cObjectUpdate() {
         {
             //zoomSizeMin = 65.0;
             if (containerScale){
-                zoomSizeMax = 75.0; //65
+                zoomSizeMax = 65.0; //65
                 growSpeed = 0.90;
             }
             else{
